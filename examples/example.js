3cc9dd80ae4f22a0b1c8ef492ee046eb028f5287
Alan P Currie <alanc909@outlook.com>
2019-01-13 18:08:00 +0000

    :cross-mark: removed example files

--- examples/example.js
+++ examples/example.js
@@ -1,3 +1,6 @@
-const code = {
-  example: ["one", "two"]
-};
+/// AAA pattern
+const unitTest = (testTitle, () => {
+// The Arrange section of a unit test method initializes objects and sets the value of the data that is passed to the method under test.
+// The Act section invokes the method under test with the arranged parameters.
+// The Assert section verifies that the action of the method under test behaves as expected.
+});
