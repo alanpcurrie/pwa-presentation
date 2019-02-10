02040d71c06b3f6bd6ab1b1984f4f88d764601b1
Alan P Currie <alanc909@outlook.com>
2019-02-10 14:12:05 +0000

    :construction: cleanup

--- examples/example.js
+++ examples/example.js
@@ -4,3 +4,28 @@ const unitTest = (testTitle, () => {
 // The Act section invokes the method under test with the arranged parameters.
 // The Assert section verifies that the action of the method under test behaves as expected.
 });
+
+/**
+ * @description simple js test
+ */
+
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+
+let result = sum(3, 7);
+let expected = 10;
+
+if (result === expected) {
+    console.log(`tests pass ✅`);
+} else {
+    throw new Error(`${result} is not equal to ${expected}`);
+}
+
+result = subtract(7, 3);
+expected = 4;
+
+if (result === expected) {
+    console.log(`tests pass ✅`);
+} else {
+    throw new Error(`${result} is not equal to ${expected}`);
+}
